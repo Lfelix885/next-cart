7367120f868c9a49c71c137aed02cb0151ed9847
leonardo.felix <leonardo.felix@efabrika.com.br>
2022-08-29 21:32:34 -0300

    arrumando bug do reload

--- src/components/OrdersTable/index.tsx
+++ src/components/OrdersTable/index.tsx
@@ -1,4 +1,10 @@
-import { ProfileImage, TableContainer, Title, Subtitle, TableContent } from "./styles";
+import {
+  ProfileImage,
+  TableContainer,
+  Title,
+  Subtitle,
+  TableContent,
+} from "./styles";
 import { profilePics } from "../../utils/profilepics";
 import { Button } from "../Button";
 
@@ -6,29 +12,25 @@ function getRandomProfileImage(items: [string]) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-interface Bla{
+interface Bla {
   id: number;
-  numProtocolo:string,
-  dataEntrada:string,
-  dataVencimento:string,
-  apresentante:string,
-  tipoProtocolo:string,
+  numProtocolo: string;
+  dataEntrada: string;
+  dataVencimento: string;
+  apresentante: string;
+  tipoProtocolo: string;
 }
-interface TableProps{
-  data:Bla[];
+interface TableProps {
+  data: Bla[];
 }
 
-export function OrdersTable({data}:TableProps) {
-
-  console.log(data)
-
+export function OrdersTable({ data }: TableProps) {
   return (
     <>
       <TableContainer>
-        {
-          data.map(x => {
-            return(
-              <TableContent key={x.id} className="row">
+        {data.map((x) => {
+          return (
+            <TableContent key={x.id} className="row">
               <div className="col-sm-2 d-flex justify-content-start align-items-center">
                 <ProfileImage>
                   <img
@@ -38,24 +40,27 @@ export function OrdersTable({data}:TableProps) {
                   ></img>
                 </ProfileImage>
               </div>
-      
+
               <div className="col align-items-center">
-                <Title className="row justify-content-center">Número do Protocolo: {x.numProtocolo}</Title>
-                <Subtitle className="row justify-content-center">Data de entrada: {x.dataEntrada}</Subtitle>
-                <Subtitle className="row justify-content-center">Data de Vencimento: {x.dataVencimento}</Subtitle>
+                <Title className="row justify-content-center">
+                  Número do Protocolo: {x.numProtocolo}
+                </Title>
+                <Subtitle className="row justify-content-center">
+                  Data de entrada: {x.dataEntrada}
+                </Subtitle>
+                <Subtitle className="row justify-content-center">
+                  Data de Vencimento: {x.dataVencimento}
+                </Subtitle>
               </div>
-      
+
               <div className="col-sm-2 d-flex justify-content-end">
                 <Button types={"tableButton"}>
                   <i className="bi bi-pencil-square"></i>
                 </Button>
               </div>
-              </TableContent>
-            )
-          })
-        }
-     
-       
+            </TableContent>
+          );
+        })}
       </TableContainer>
     </>
   );
